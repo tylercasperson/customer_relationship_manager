6518b29076ff5c6acd26b54c42b9278c1d9cb510
Tyler Casperson <tylercasperson@gmail.com>
2020-07-30 23:28:43 -0600

    restructured the folders based on the tutorials I participated in

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,13 @@ import './App.css';
 
 function App() {
   return (
-    <div id="BusinessRealtionshipManager">
-      <div className="container-fluid router-body">
+    <div id='BusinessRealtionshipManager'>
+      <div className='container-fluid router-body'>
         <Router>
           <div>
             <Switch>
-              <Route exact path="/" component={Businesses} />
-              <Route exact path="/businesses" component={Businesses} />
+              <Route exact path='/' component={Businesses} />
+              <Route exact path='/businesses' component={Businesses} />
             </Switch>
           </div>
         </Router>
