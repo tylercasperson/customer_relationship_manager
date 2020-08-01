5571650227e897a050943fbc4970bee3604a3450
Tyler Casperson <tylercasperson@gmail.com>
2020-07-31 22:05:33 -0600

    installed more packages so the frontend and backend can run concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Businesses from './pages/Businesses';
 import './App.css';
 
 function App() {
   return (
     <div id='BusinessRealtionshipManager'>
       <div className='container-fluid router-body'>
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path='/' component={Businesses} />
-              <Route exact path='/businesses' component={Businesses} />
-            </Switch>
-          </div>
-        </Router>
+        <h1>Customer Relationship Manager</h1>
       </div>
     </div>
   );
