b3dd0cd4cc265ab2da789d9f6330d9602445573a
Tyler Casperson <tylercasperson@gmail.com>
2020-08-02 22:27:30 -0600

    setup product, contact and service

---
+++ client/src/components/layout/Service.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Service = () => {
+  return (
+    <div className='w-24 rounded-lg bg-blue-600 text-white text-center p-2 overflow-hidden'>
+      Custom Products
+    </div>
+  );
+};
+
+export default Service;
