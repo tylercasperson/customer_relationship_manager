ed12b38237e1fd67ae062d0cb3e828a2ed285e1f
Tyler Casperson <tylercasperson@gmail.com>
2020-08-08 22:14:26 -0600

    placed default links to all of the social sites

--- client/src/components/layout/businessCard/SocialLinks.js
+++ client/src/components/layout/businessCard/SocialLinks.js
@@ -3,28 +3,28 @@ import React from 'react';
 const SocialLinks = (props) => {
   return (
     <div className='flex px-3 py-2 item-center justify-between'>
-      <a href='#!'>
+      <a href='https://www.youtube.com/'>
         <i className='fab fa-youtube'></i>
       </a>
-      <a href='#!'>
+      <a href='https://twitter.com/'>
         <i className='fab fa-twitter'></i>
       </a>
-      <a href='#!'>
+      <a href='https://www.amazon.com/'>
         <i className='fab fa-amazon'></i>
       </a>
-      <a href='#!'>
+      <a href='https://www.facebook.com/'>
         <i className='fab fa-facebook-square'></i>
       </a>
-      <a href='#!'>
+      <a href='https://www.instagram.com/'>
         <i className='fab fa-instagram'></i>
       </a>
-      <a href='#!'>
+      <a href='https://www.linkedin.com/'>
         <i className='fab fa-linkedin'></i>
       </a>
-      <a href='#!'>
+      <a href='https://www.tiktok.com/'>
         <i className='fab fa-tiktok'></i>
       </a>
-      <a href='#!'>
+      <a href='https://www.whatsapp.com/'>
         <i className='fab fa-whatsapp'></i>
       </a>
     </div>
