098ae90928fd3bb51c6913fcd7b13390b45495c9
Tyler Casperson <tylercasperson@gmail.com>
2020-09-14 19:10:08 -0600

    event reducer and state match routes created

---
+++ client/src/context/event/EventState.js
@@ -0,0 +1,173 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import EventContext from './eventContext';
+import eventReducer from './eventReducer';
+import {
+  GET_EVENTS,
+  CREATE_EVENT,
+  UPDATE_EVENT,
+  DELETE_ATTENDANCE_TO_EVENT,
+  DELETE_EVENT,
+  EVENT_ERROR,
+} from '../types';
+
+const EventState = (props) => {
+  const initialState = {
+    events: [],
+    eventError: [],
+  };
+
+  const [state, dispatch] = useReducer(eventReducer, initialState);
+
+  // Get Events
+  const getEvents = async () => {
+    try {
+      const res = await axios.get('/api/events');
+
+      dispatch({
+        type: GET_EVENTS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: EVENT_ERROR,
+        payload: err.response,
+      });
+    }
+  };
+
+  // Get events that businesses attend
+  const getEventsForBusinesses = async (eventId) => {
+    try {
+      const res = await axios.get(`/api/events/${eventId}`);
+
+      dispatch({
+        type: GET_EVENTS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: EVENT_ERROR,
+        payload: err.response,
+      });
+    }
+  };
+
+  // Get Business specific events
+  const getBusinessEvents = async (businessId) => {
+    try {
+      const res = await axios.get(`/api/events/${businessId}`);
+
+      dispatch({
+        type: GET_EVENTS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: EVENT_ERROR,
+        payload: err.response,
+      });
+    }
+  };
+
+  // Add Event
+  const createEvent = async (event) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/events', event, config);
+
+      dispatch({
+        type: CREATE_EVENT,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: EVENT_ERROR,
+        payload: err,
+      });
+    }
+  };
+
+  // Update Event
+  const updateEvent = async (event) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(`/api/events/${event.id}`, event, config);
+
+      dispatch({
+        type: UPDATE_EVENT,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: EVENT_ERROR,
+        payload: err,
+      });
+    }
+  };
+
+  // Delete attendance to the Event
+  const deleteAttendanceToEvent = async (eventId, businessId) => {
+    try {
+      await axios.delete(`/api/events/${eventId}/${businessId}`);
+
+      dispatch({
+        type: DELETE_ATTENDANCE_TO_EVENT,
+        payload: eventId,
+        businessId,
+      });
+    } catch (err) {
+      dispatch({
+        type: EVENT_ERROR,
+        payload: err,
+      });
+    }
+  };
+
+  // Delete the Event
+  const deleteEvent = async (eventId) => {
+    try {
+      await axios.delete(`/api/events/${eventId}`);
+
+      dispatch({
+        type: DELETE_EVENT,
+        payload: eventId,
+      });
+    } catch (err) {
+      dispatch({
+        type: EVENT_ERROR,
+        payload: err,
+      });
+    }
+  };
+
+  return (
+    <EventContext.Provider
+      value={{
+        events: state.events,
+        eventError: state.eventError,
+        getEvents,
+        getEventsForBusinesses,
+        getBusinessEvents,
+        createEvent,
+        updateEvent,
+        deleteAttendanceToEvent,
+        deleteEvent,
+      }}
+    >
+      {props.children}
+    </EventContext.Provider>
+  );
+};
+
+export default NoteState;
--- client/src/context/event/eventReducer.js
+++ client/src/context/event/eventReducer.js
@@ -1,7 +1,55 @@
-import {} from '../types';
+import {
+  GET_EVENTS,
+  CREATE_EVENT,
+  UPDATE_EVENT,
+  DELETE_ATTENDANCE_TO_EVENT,
+  DELETE_EVENT,
+  EVENT_ERROR,
+} from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_EVENTS:
+      return {
+        ...state,
+        events: action.payload,
+        loading: false,
+      };
+    case CREATE_EVENT:
+      return {
+        ...state,
+        events: [...state.events, action.payload],
+        loading: false,
+      };
+    case UPDATE_EVENT:
+      return {
+        ...state,
+        events: state.events.map((event) =>
+          event.id === action.payload.id ? action.payload : event
+        ),
+      };
+
+    case DELETE_ATTENDANCE_TO_EVENT:
+      return {
+        ...state,
+        events: state.events.filter(
+          (event, business) =>
+            event.id !== action.payload[0] && business.id !== action.payload[1]
+        ),
+        loading: false,
+      };
+
+    case DELETE_EVENT:
+      return {
+        ...state,
+        events: state.events.filter((event) => event.id !== action.payload),
+        loading: false,
+      };
+    case EVENT_ERROR:
+      return {
+        ...state,
+        eventError: action.payload,
+      };
     default:
       return state;
   }
